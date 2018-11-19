2968a44ddbea41669d965d330d7fa176ccfb913c
vitor.silva <vitor.silva@mme.gov.br>
2018-11-19 10:09:03 -0200

    ADD

---
+++ js/Controllers/gerirInfosBasicasContoller.js
@@ -0,0 +1,12 @@
+angular
+    .module('inspinia')
+    .controller('GerirInfosBasicasCtrl', function GerirInfosBasicasCtrl($scope) {
+
+
+ $scope.cadastrar=function(){ 
+  alert("chamou");    
+ };
+ 
+ 
+ 
+});
